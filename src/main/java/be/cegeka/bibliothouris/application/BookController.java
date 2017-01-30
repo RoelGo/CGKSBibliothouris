055c384cd9591730be40f6a675f857ff2331421a
Christoph <christoph.marckx@cegeka.com>
2017-01-30 16:40:37 +0100

    Adding more stories

--- src/main/java/be/cegeka/bibliothouris/application/BookController.java
+++ src/main/java/be/cegeka/bibliothouris/application/BookController.java
@@ -2,11 +2,10 @@ package be.cegeka.bibliothouris.application;
 
 import be.cegeka.bibliothouris.domain.books.Book;
 import be.cegeka.bibliothouris.domain.books.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
 import javax.xml.bind.ValidationException;
@@ -39,5 +38,39 @@ public class BookController {
             e.printStackTrace();
         }
     }
+
+    @RequestMapping(value = "/search/{isbn}", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    ResponseEntity getBooksByISBN(@PathVariable("isbn") String ISBN) {
+        try {
+            return new ResponseEntity<>(bookService.searchStringByIsbn(ISBN), HttpStatus.ACCEPTED);
+        } catch (ValidationException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
+
+    @RequestMapping(value = "/search/{author}", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    ResponseEntity getBooksByauthor(@PathVariable("author") String author) {
+        try {
+            return new ResponseEntity<>(bookService.searchStringByAuthor(author), HttpStatus.ACCEPTED);
+        } catch (ValidationException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
+
+    @RequestMapping(value = "/search/{title}", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    ResponseEntity getBooksBytitle(@PathVariable("title") String title) {
+        try {
+            return new ResponseEntity<>(bookService.searchStringByIsbn(title), HttpStatus.ACCEPTED);
+        } catch (ValidationException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
+
 
 }
--- src/main/java/be/cegeka/bibliothouris/domain/books/BookService.java
+++ src/main/java/be/cegeka/bibliothouris/domain/books/BookService.java
@@ -28,12 +28,69 @@ public class BookService {
 
     }
 
+    public String getBooksByCriteria(String criteria){
+        String testCriteria = criteria.replaceAll("\\*",".+");
+        if (testCriteria.length() > 1){
+            if (testCriteria.substring(0,2).equals(".+")){
+                testCriteria = ".+" + criteria.substring(2,testCriteria.length());
+            }
+            if (testCriteria.substring(testCriteria.length() - 2, testCriteria.length()).equals(".+")){
+                testCriteria = criteria.substring(0,testCriteria.length()-2)+".*";
+            }
+        }
+        return testCriteria;
+    }
+
+    public List<Book> searchStringByIsbn(String isbn) throws ValidationException {
+        List<Book> foundBooks = new ArrayList<>();
+        String testIsbn = getBooksByCriteria(isbn);
+        for (Book book : bookRepository.getAllBooks()) {
+            if (book.getISBN().matches(testIsbn)) {
+                foundBooks.add(book);
+            }
+            if (foundBooks.isEmpty()) {
+                throw new ValidationException("No matching ISBN was found.");
+            }
+        }
+        return foundBooks;
+    }
+
+    public List<Book> searchStringByAuthor(String Author) throws ValidationException {
+        List<Book> foundBooks = new ArrayList<>();
+        String testAuthor = getBooksByCriteria(Author);
+        for (Book book : bookRepository.getAllBooks()) {
+            if (book.getAuthorFirstName().matches(testAuthor) || book.getAuthorLastName().matches(testAuthor) ) {
+                foundBooks.add(book);
+            }
+            if (foundBooks.isEmpty()) {
+                throw new ValidationException("No matching Author was found.");
+            }
+        }
+        return foundBooks;
+    }
+
+    public List<Book> searchStringByTitle(String Title) throws ValidationException {
+        List<Book> foundBooks = new ArrayList<>();
+        String testTitle = getBooksByCriteria(Title);
+        for (Book book : bookRepository.getAllBooks()) {
+            if (book.getTitle().matches(testTitle)) {
+                foundBooks.add(book);
+            }
+            if (foundBooks.isEmpty()) {
+                throw new ValidationException("No matching Title was found.");
+            }
+        }
+        return foundBooks;
+    }
+
     public List<Book> getAllBooks() {
         return bookRepository.getAllBooks();
     }
 
 
-    public List<Book> searchBook(String searchInput) {
+
+
+   /* public List<Book> searchBook(String searchInput) {
         List<Book> searchList = bookRepository.getAllBooks();
         List<Book> hitList = new ArrayList<>();
         for (Book book : searchList) {
@@ -52,6 +109,6 @@ public class BookService {
         return (book.getISBN().matches("(.*)" + searchInput + "(.*)") ||
                 book.getAuthorFirstName().matches("(.*)" + searchInput + "(.*)") ||
                book.getAuthorLastName().matches("(.*)" + searchInput + "(.*)"));
-    }
+    }*/
 
 }
